105aedfe2b6ad5c74258e2837a5526b134b5e23d
Mike O'Donnell <mxo7550@rit.edu>
2017-10-28 19:07:28 -0400

    Added a prototype Bark. Whenever E is pressed, the enemy will stop.

--- owo/Assets/Scripts/EnemyMove.cs
+++ owo/Assets/Scripts/EnemyMove.cs
@@ -8,14 +8,27 @@ public class EnemyMove : MonoBehaviour {
     //variables used for speed, speed will only be set to 0 for a second when the dog barks
     public float walkSpeed;
     private float currentSpeed;
+    //Timer used to freeze the bad guy on Bark
+    private int stopTimer;
     void Start ()
     {
         currentSpeed = walkSpeed;
+        stopTimer = 61;
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
+        //Move the dude
         transform.Translate(currentSpeed * Time.deltaTime, 0, 0);
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            currentSpeed = 0;
+            stopTimer = 0;
+        }
+        if (stopTimer == 60)
+            currentSpeed = walkSpeed;
+        stopTimer++;
+            
     }
 }
