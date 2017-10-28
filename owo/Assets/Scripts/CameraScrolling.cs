53b2dd5f004ce97bf2d6619086df3a0f002f1ee2
DESKTOP-I7HIVQU\Shanno <seh6271@g.rit.edu>
2017-10-28 02:46:03 -0400

    Moved old commits to new repo with gitignore

---
+++ owo/Assets/Scripts/CameraScrolling.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScrolling : MonoBehaviour {
+
+    public float walkSpeed;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        ConstantMovement();
+	}
+
+    public void ConstantMovement()
+    {
+        transform.Translate(walkSpeed * Time.deltaTime, 0, 0);
+        //rBody.MovePosition((rBody.transform.position + new Vector3(walkSpeed, 0,0)));
+    }
+}
---
+++ owo/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//controls players movement
+public class PlayerMovement : MonoBehaviour {
+  
+    private Rigidbody2D rBody;
+
+    public float velocity;
+    public float walkSpeed;
+
+	// Use this for initialization
+	void Start () {
+        rBody = gameObject.GetComponent<Rigidbody2D>();        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        ConstantMovement();
+
+        //jump check
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            rBody.AddRelativeForce(Vector2.up * rBody.mass * (velocity * 200) * Time.deltaTime);            
+        }       
+    }
+
+    public void ConstantMovement()
+    {
+        transform.Translate(walkSpeed * Time.deltaTime, 0, 0);
+        //rBody.MovePosition((rBody.transform.position + new Vector3(walkSpeed, 0,0)));
+    }
+
+    
+}
