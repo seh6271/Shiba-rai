8e039adc4292452d9a97ab86325dc3dd24bbf90f
DESKTOP-I7HIVQU\Shanno <seh6271@g.rit.edu>
2017-10-28 14:10:54 -0400

    SmoothCamera in y, variable changes, new prefab

--- owo/Assets/Scripts/CameraScrolling.cs
+++ owo/Assets/Scripts/CameraScrolling.cs
@@ -4,22 +4,37 @@ using UnityEngine;
 
 public class CameraScrolling : MonoBehaviour {
 
-    public float walkSpeed;
+    public float scollSpeed;
+    public GameObject player;
+    private float raise;
+
+    public float dampTime = 0.15f;
+    private Vector3 velocity = Vector3.zero;
+    public Transform target;
 
     // Use this for initialization
     void Start () {
-		
+        target = player.GetComponent<Transform>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
+        Follow();
         ConstantMovement();
 	}
 
     public void ConstantMovement()
     {
-        transform.Translate(walkSpeed * Time.deltaTime, 0, 0);
-        //rBody.MovePosition((rBody.transform.position + new Vector3(walkSpeed, 0,0)));
+        transform.Translate(scollSpeed * Time.deltaTime, 0, 0);        
+    }
+
+    public void Follow()
+    {
+        if (target)
+        {
+            raise = target.position.y - transform.position.y;
+            transform.Translate(0, raise * 3 * Time.deltaTime, 0);
+        }
     }
 }
