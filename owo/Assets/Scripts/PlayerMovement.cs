054c9abca8d0002b8aded7e11c38f33173c4da06
Shannon <seh6271@g.rit.edu>
2017-10-29 04:21:27 -0400

    animation fixes?

--- owo/Assets/Scripts/PlayerMovement.cs
+++ owo/Assets/Scripts/PlayerMovement.cs
@@ -17,6 +17,9 @@ public class PlayerMovement : MonoBehaviour {
     private float crouchForwardSpeed;
     private float crouchBackwardSpeed;
 
+    public static int animNum;
+    private bool police;
+
     private Animator animator;
 
 	// Use this for initialization
@@ -29,6 +32,8 @@ public class PlayerMovement : MonoBehaviour {
         crouchForwardSpeed = crouchSpeed + (increment / 3f);
         crouchBackwardSpeed = crouchSpeed - (increment / 3f) - 5;
         currentSpeed = walkSpeed;
+        animNum = 0;
+        police = true;
     }
 	
 	// Update is called once per frame
@@ -36,8 +41,26 @@ public class PlayerMovement : MonoBehaviour {
 
         if (rBody.velocity.y == 0)
         {
-            animator.SetInteger("Action", 0);
+            animNum = 0;
+            animator.SetInteger("Action", animNum);
         }
+
+
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            police = !police;
+        }
+
+        if(!police && animNum < 3)
+        {
+            animNum += 3;            
+        }
+        else if(police && animNum > 2)
+        {
+            animNum -= 3;
+        }
+
+        animator.SetInteger("Action", animNum);
 
         ConstantMovement();
 
@@ -53,33 +76,53 @@ public class PlayerMovement : MonoBehaviour {
 
     public void CheckInput()
     {
-       
+
         //jump check
         if (Input.GetKeyDown(KeyCode.W) && (rBody.velocity.y < .5 && rBody.velocity.y > -.3))
         {
             rBody.AddRelativeForce(Vector2.up * rBody.mass * (velocity * 200) * Time.deltaTime);
-            animator.SetInteger("Action", 1);
+            animNum = 1;
+            animator.SetInteger("Action", animNum);
         }
         //walk check
+        if (Input.GetKey(KeyCode.S))
+        { 
+            animNum = 2;
+            animator.SetInteger("Action", animNum);
+            currentSpeed = crouchSpeed;            
+            return;
+        }      
         if (Input.GetKey(KeyCode.S) && Input.GetKey(KeyCode.D))
+        {
             currentSpeed = crouchForwardSpeed;
-        else if (Input.GetKey(KeyCode.D))
+            animNum = 2;
+            animator.SetInteger("Action", animNum);
+            return;
+        }
+        if (Input.GetKey(KeyCode.D))
         {
             currentSpeed = forwardSpeed;
+            animNum = 0;
+            animator.SetInteger("Action", animNum);
+            return;
         }
-        else if (Input.GetKey(KeyCode.S) && Input.GetKey(KeyCode.A))
+        if (Input.GetKey(KeyCode.S) && Input.GetKey(KeyCode.A))
+        {
             currentSpeed = crouchBackwardSpeed;
-        else if (Input.GetKey(KeyCode.A))
+            animNum = 2;
+            animator.SetInteger("Action", animNum);
+            return;
+        }
+        if (Input.GetKey(KeyCode.A))
         {
             currentSpeed = backwardsSpeed;
-        }
-       
-        else if (Input.GetKey(KeyCode.S))
-            currentSpeed = crouchSpeed;
-        else
-        {
-            currentSpeed = walkSpeed;
-        }
+            animNum = 0;
+            animator.SetInteger("Action", animNum);
+            return;
+        }        
+
+        currentSpeed = walkSpeed;
+        
     }
 
     
