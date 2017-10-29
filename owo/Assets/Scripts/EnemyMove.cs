f7196a1116df9486b282b8685e705da6e4d86dc6
DESKTOP-I7HIVQU\Shanno <seh6271@g.rit.edu>
2017-10-28 20:27:40 -0400

    deleted merge markers

--- owo/Assets/Scripts/EnemyMove.cs
+++ owo/Assets/Scripts/EnemyMove.cs
@@ -8,26 +8,22 @@ public class EnemyMove : MonoBehaviour {
     //variables used for speed, speed will only be set to 0 for a second when the dog barks
     public float walkSpeed;
     private float currentSpeed;
-<<<<<<< .merge_file_a15068
-    void Start ()
-    {
-        currentSpeed = walkSpeed;
-=======
+
     //Timer used to freeze the bad guy on Bark
     private int stopTimer;
     void Start ()
     {
         currentSpeed = walkSpeed;
         stopTimer = 61;
->>>>>>> .merge_file_a08188
+
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
-<<<<<<< .merge_file_a15068
+
         transform.Translate(currentSpeed * Time.deltaTime, 0, 0);
-=======
+
         //Move the dude
         transform.Translate(currentSpeed * Time.deltaTime, 0, 0);
         if (Input.GetKeyDown(KeyCode.E))
@@ -39,6 +35,6 @@ public class EnemyMove : MonoBehaviour {
             currentSpeed = walkSpeed;
         stopTimer++;
             
->>>>>>> .merge_file_a08188
+
     }
 }
